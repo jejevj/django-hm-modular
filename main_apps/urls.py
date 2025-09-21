a57db2b667939024c8065b19a9939ce657930542
J Angga Wijaya <wijaya.angga.j@gmail.com>
2025-09-21 16:44:33 +0700

    app 1.1 engine view clear

--- main_apps/urls.py
+++ main_apps/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('test', views.get, name='get'),
+    # Adding the module URL Path    
+    path('module/', views.module_list_view, name='module-list'),
+    path('module/install/<str:module_name>/', views.install_module, name='install-module'),
 ]
--- main_apps/views.py
+++ main_apps/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+import os, json
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 
 def get(request):
     return HttpResponse("Hello world!")
+
+# modules/views.py
+
+MODULES_DIR = os.path.join(settings.BASE_DIR, 'main_apps', 'modules_list')
+
+def module_list_view(request):
+    modules = []
+
+    for module_name in os.listdir(MODULES_DIR):
+        module_path = os.path.join(MODULES_DIR, module_name)
+        module_json_path = os.path.join(module_path, 'module.json')
+
+        if os.path.exists(module_json_path):
+            with open(module_json_path) as f:
+                data = json.load(f)
+                data['name'] = module_name
+                modules.append(data)
+
+    return render(request, 'modules/module_list.html', {'modules': modules})
+
+
+def install_module(request, module_name):
+    try:
+        module_path = os.path.join(MODULES_DIR, module_name, 'install.py')
+        exec(open(module_path).read(), {})
+        messages.success(request, f"Module '{module_name}' installed successfully.")
+    except Exception as e:
+        messages.error(request, f"Error installing module: {str(e)}")
+
+    return redirect('module-list')
