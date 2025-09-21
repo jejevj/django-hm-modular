6e92d540ba11382d05bf1fed30aef357f6ab614e
J Angga Wijaya <wijaya.angga.j@gmail.com>
2025-09-21 17:47:43 +0700

    check path url tidak boleh sama

--- main_apps/models.py
+++ main_apps/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class Module(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    url_path = models.CharField(max_length=100)
+    url_path = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text="Url path sudah digunakan, harus unik. hanya huruf kecil, angka, hyphens (-), dan slashes (/) yang diperbolehkan."
+    )
     description = models.TextField(blank=True, null=True)
     version = models.CharField(max_length=10, default='1.0')
     is_active = models.BooleanField(default=True) 
