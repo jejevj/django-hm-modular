932f21493964afaab93ae2f016eab490cd3499ad
J Angga Wijaya <wijaya.angga.j@gmail.com>
2025-09-21 16:31:20 +0700

    setting & url configurations

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)7)q$d6n7ebrl*5prubrt3nca5og(r(ai5^mnf4syvomlni^zw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_apps',
 ]
 
 MIDDLEWARE = [
